1a2613a18f998418dc911c2a74b6f650efdf6dcd
Dylan Morrison <dizzy@domad.science>
2023-06-07 03:55:03 -0400

    Comment playback.cpp, point out where I don't understand the code.

--- source/playback.cpp
+++ source/playback.cpp
@@ -15,19 +15,19 @@ static CondVar aStatusCV;
 static int current_samplerate=AUDIO_SAMPLERATE;
 
 void start_playback() {
-	atb = (u32 *)malloc(ATB_SIZE * sizeof(u32));
-	mutexInit(&aLock);
-	mutexInit(&aStatusLock);
-	condvarInit(&aStatusCV, &aStatusLock);
-	audoutInitialize();
-	audoutStartAudioOut();
+	atb = (u32 *)malloc(ATB_SIZE * sizeof(u32)); // Allocate audio buffer
+	mutexInit(&aLock); // Lock audio mutex
+	mutexInit(&aStatusLock); // Lock tag mutex
+	condvarInit(&aStatusCV, &aStatusLock); // Tie audio status mutex state to audio status conditional variable
+	audoutInitialize(); // Initialize output state
+	audoutStartAudioOut(); // Start output
 	
 	playing = true;
-	threadCreate(&playback_thread, playback_thread_main, NULL, 0x10000, 0x2B, 2);
-	threadStart(&playback_thread);
+	threadCreate(&playback_thread, playback_thread_main, NULL, 0x10000, 0x2B, 2); // Create playback thread
+	threadStart(&playback_thread); // Start playback thread
 }
 
-void set_samplerate(int sr) {
+void set_samplerate(int sr) { // Lock audio mutex, set samplerate, unlock mutex.
 	mutexLock(&aLock);
 	current_samplerate = sr;
 	mutexUnlock(&aLock);
@@ -45,6 +45,7 @@ void stop_playback(bool playout) {
 	//       isn't noticeable anyway) than have people testing the
 	//       release end up corrupting their filesystems from a hard freeze
 	//       with open handles, as has happened to be repeatedly now.
+        // ED:   Is this bug even still active? I thought I'd fixed resampling.
 	if (playout && (current_samplerate==AUDIO_SAMPLERATE)) {
 		bool flsh = false;
 		while (!flsh) { // Wait for the buffer to empty before terminating.
@@ -56,7 +57,8 @@ void stop_playback(bool playout) {
 		u64 sleepnano = 4000000000/AUDIO_BUFFER_DIVIDER; // Sleep for 4x the system buffer size, to drain all buffers.
 		svcSleepThread(sleepnano);
 	}
-	
+	// Lock the audio status mutex, wait until it's ready to be unlocked, set playing to false,
+        //  wait for the playback thread to exit, then stop the audio output
 	mutexLock(&aStatusLock);
 	condvarWait(&aStatusCV);
 	playing = false;
@@ -66,7 +68,7 @@ void stop_playback(bool playout) {
 	audoutExit();
 }
 
-void abort_playback() { // Used for errors.
+void abort_playback() { // Used for errors. Essentially does stop_playback without the flushing. Should maybe combine the two functions.
 	mutexLock(&aStatusLock);
 	condvarWait(&aStatusCV);
 	playing = false;
@@ -76,11 +78,11 @@ void abort_playback() { // Used for errors.
 	audoutExit();
 }
 
-void playback_thread_main(void *) {
+void playback_thread_main(void *) { // Main playback thread
 	bool waiting = true;
-	while (waiting) {
+	while (waiting) { // Wait until at least half the buffer is unused before adding more to it.
 		svcSleepThread(1000);
-		if (atbUsed > (ATB_SIZE/2)) {
+		if (atbUsed > (ATB_SIZE/2)) { 
 			waiting = false;
 		}
 		mutexLock(&aStatusLock);
@@ -91,62 +93,62 @@ void playback_thread_main(void *) {
 	}
 	AudioOutBuffer sources[4];	
 	
-	u32 rdata_size = (AUDIO_BUFFER_SAMPLES * sizeof(u32) + 0xfff) & ~0xfff;
-	u32 *rdata[4];
+	u32 rdata_size = (AUDIO_BUFFER_SAMPLES * sizeof(u32) + 0xfff) & ~0xfff; // Set buffer size for raw data. Not sure why the + 0xfff & ~0xfff? Extra space, but aligned to 0x1000 I guess.
+	u32 *rdata[4]; // Also not sure why 4 buffers. Presumably this is just how the switch prefers its audio?
 	for (int i = 0; i < 4; i++) {
-		rdata[i] = (u32 *)memalign(0x1000, rdata_size);
-		memset(rdata[i], 0, rdata_size);
-		sources[i].next = 0;
-		sources[i].buffer = rdata[i];
-		sources[i].buffer_size = rdata_size;
+		rdata[i] = (u32 *)memalign(0x1000, rdata_size); // Allocate 4kb of aligned memory for each buffer
+		memset(rdata[i], 0, rdata_size); // Clear this buffer
+		sources[i].next = 0; 
+		sources[i].buffer = rdata[i]; // Also clear and set the size of the audio output buffers. These are *not* padded.
+		sources[i].buffer_size = rdata_size; 
 		sources[i].data_size = AUDIO_BUFFER_SAMPLES * sizeof(u32);
 		sources[i].data_offset = 0;
 		
-		audoutAppendAudioOutBuffer(&sources[i]);
+		audoutAppendAudioOutBuffer(&sources[i]); // Add all 4 buffers to the audio output
 	}
 	mutexLock(&aStatusLock);
 	bool p = playing;
 	flushing = false;
 	while (p) {
-		condvarWakeAll(&aStatusCV);
+		condvarWakeAll(&aStatusCV); // Wait until the Audio Status thread is ready to be unlocked, then unlock it
 		mutexUnlock(&aStatusLock);
 		
-		u32 cnt;
+		u32 cnt; // Count of played buffers
 		AudioOutBuffer *released;
-		audoutWaitPlayFinish(&released, &cnt, U64_MAX);
+		audoutWaitPlayFinish(&released, &cnt, U64_MAX); // Wait for playback to finish of current buffers, get a count of how many are available.
 
-		mutexLock(&aLock);		
+		mutexLock(&aLock); // Lock the audio mutex
 		if (atbUsed > 0) {
-			mutexLock(&aStatusLock);
+			mutexLock(&aStatusLock); // Lock the audio status mutex, say that we're no longer flushing buffers, unlock it.
 			flushing = false;
 			condvarWakeAll(&aStatusCV);
 			mutexUnlock(&aStatusLock);
 			u32 size;
 			
-			if (atbUsed < AUDIO_BUFFER_SAMPLES) {
+			if (atbUsed < AUDIO_BUFFER_SAMPLES) { // If the internal buffer is less than the max number of buffer samples per the samplerate, only send what's there.
 				size = atbUsed * sizeof(u32);
 			} else {
-				size = AUDIO_BUFFER_SAMPLES * sizeof(u32);
+				size = AUDIO_BUFFER_SAMPLES * sizeof(u32); // If it's not, send all samples for the (48000Hz on the switch) samplerate.
 			}
 			
 			if (AUDIO_SAMPLERATE == current_samplerate) { // If we're matched samplerate, just blast right into the switch.
-				memcpy(released->buffer, atb, size);
-				if (size == 0) {
+				memcpy(released->buffer, atb, size); // Copy from the internal buffer into the released Switch buffers.
+				if (size == 0) { // Not sure why this is here? Should be covered by the above if/else, size should never be 0.
 					released->data_size = AUDIO_BUFFER_SAMPLES * sizeof(u32);
 				} else {
 					released->data_size = size;
-				}
-				atbUsed -= size / sizeof(u32);
-				memmove(atb, atb + (size / sizeof(u32)), atbUsed * sizeof(u32));
+				} // Regardless, set the data size of the released buffer to the amount of samples we just pushed.
+				atbUsed -= size / sizeof(u32); // Reduce the used amount of the internal ring buffer.
+				memmove(atb, atb + (size / sizeof(u32)), atbUsed * sizeof(u32)); // Slide the ring buffer
 			} else {
-				resampleBuffer((short*)released->buffer, (size / sizeof(u32)));
-				int outsize = (size * current_samplerate) / (sizeof(u32) * AUDIO_SAMPLERATE); 
+				resampleBuffer((short*)released->buffer, (size / sizeof(u32))); // Resample audio to Switch samplerate, push into released buffers.
+				int outsize = (size * current_samplerate) / (sizeof(u32) * AUDIO_SAMPLERATE); // Get the size of the interpolated output
 				atbUsed -= outsize;
 				if (atbUsed<0) atbUsed=0;
-				memmove(atb, atb + outsize, atbUsed * sizeof(u32));
+				memmove(atb, atb + outsize, atbUsed * sizeof(u32)); // Slide the ring buffer
 			}
-			audoutAppendAudioOutBuffer(released);
-		} else {
+			audoutAppendAudioOutBuffer(released); // Append the released buffers with their new contents to the Switch audio buffers
+		} else { // If there's nothing in the internal ring buffer, flush the Switch buffers then shut down the playback thread.
 			mutexLock(&aStatusLock);
 			flushing = true;
 			condvarWakeAll(&aStatusCV);
@@ -164,11 +166,12 @@ void playback_thread_main(void *) {
 		p = playing;
 	}
 	
+	// Why am I freeing only two of these? Didn't I use 4?
 	free(rdata[0]);
 	free(rdata[1]);
 }
 
-int fillPlayBuffer(int16_t *inBuffer, int length) {
+int fillPlayBuffer(int16_t *inBuffer, int length) { // Fill the ring buffer with new audio.
 	mutexLock(&aLock);
 	if (atbUsed + length >= ATB_SIZE) {
 		mutexUnlock(&aLock);
@@ -180,26 +183,29 @@ int fillPlayBuffer(int16_t *inBuffer, int length) {
 	return 0;
 }
 
-
-void resampleBuffer(short* out, int numSamples) {
-	short *s_atb = reinterpret_cast<short *>(atb);
-	unsigned long long m_mixPtr=0;
-	int resampleDelta=((current_samplerate*0x40000ull)/AUDIO_SAMPLERATE);
-	
-	for(int i=0; i<numSamples; i++) {
+// This code partially by LogiCow of the MZX Discord
+void resampleBuffer(short* out, int numSamples) { // The scary function! Linearly resample/interpolate to Switch samplerate.
+	// QUESTION: What happens to this function when the samplerate is *higher* than output?
+	short *s_atb = reinterpret_cast<short *>(atb); // Recast the internal ring buffer into 16-bit shorts (per-channel) instead of 32-bit integers (both channels)
+	unsigned long long m_mixPtr=0; // Create a 64-bit pointer into the mixing buffer
+	int resampleDelta=((current_samplerate*0x40000ull)/AUDIO_SAMPLERATE); // Figure out the multiplier to turn the input samplerate to the desired samplerate.
+									      // Uncertain of the 0x40000ull value other than it exists to enable the division using a 64-bit integer,
+                                                                              // and is used to position within the mixing buffer.
+	for(int i=0; i<numSamples; i++) { // Iterate over the input samples.
 		
-		int idx = m_mixPtr >> 18;
-		int s1 = s_atb[(idx)*2];
-		int s2 = s_atb[(idx+1)*2];
-		int interp = m_mixPtr & 0x3ffff;
-		long long int sRamp = s2-s1;
-		sRamp *= interp;
-		sRamp >>= 18;
-		sRamp += s1;
-		if(sRamp > 32767) sRamp = 32767;
+		int idx = m_mixPtr >> 18; // Get an index into the source buffer by taking the mix pointer and discarding the extra bits.
+		int s1 = s_atb[(idx)*2]; // Get the 'current' left channel sample
+		int s2 = s_atb[(idx+1)*2]; // Get the 'next' left channel sample
+		int interp = m_mixPtr & 0x3ffff; // Get the actual interpolation multiplier by clearing the high bits of the mix pointer value.
+		long long int sRamp = s2-s1; // Get the difference between the two samples.
+		sRamp *= interp; // Interpolate the difference
+		sRamp >>= 18; // Bitshift to clamp.
+		sRamp += s1; // Add back the original sample.
+		if(sRamp > 32767) sRamp = 32767; // Further clamping, ensure values are not larger/smaller that signed 16-bit max/min
 		if(sRamp < -32768) sRamp = -32768;
-		out[(i*2)] = (short)sRamp;
+		out[(i*2)] = (short)sRamp; // Add the interpolated sample to the output buffer
 		
+		// Repeat for the right channel. Should this be broken out into its own function?
 		s1 = s_atb[(idx*2)+1];
 		s2 = s_atb[((idx+1)*2)+1];
 		sRamp = s2-s1;
@@ -210,6 +216,7 @@ void resampleBuffer(short* out, int numSamples) {
 		if(sRamp < -32768) sRamp = -32768;
 		out[((i*2)+1)] = (short)sRamp;
 		
+		// Move the mix pointer forwards.
 		m_mixPtr += resampleDelta;
 	}
 }
